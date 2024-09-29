3729a280bcdcef1e1107fd05065ae1b3a36ef31c
wujinfeng <wjf@chagine.com>
2024-09-29 17:33:13 +0800

    更改jdk版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     lint {
         abortOnError = false
@@ -52,7 +52,7 @@ android {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(11)
     compilerOptions {
         freeCompilerArgs.add("-Xjvm-default=all")
     }
